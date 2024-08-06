24007c43a933084ef3e06962f415d40098f89a7d
b12s309 <gomezdiezkevinmateo@gmail.com>
2024-08-06 10:44:21 -0500

    Create Classes Point and Circulo

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,14 @@
 # TODO: Add code here
+import math
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x: float = 0
+        self.y: float = 0
+
+class Circulo:
+    def __init__(self, center: Point, radius:float):
+        self.radius: float= 0
+
+    def area(self)->float:
+        Area = math.pi*self.radius
+
