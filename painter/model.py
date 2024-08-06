d2161169f15dac596329ebcc53de5c7e22743db7
b12s309 <gomezdiezkevinmateo@gmail.com>
2024-08-06 11:08:55 -0500

    Create class circule with area, draw

--- painter/model.py
+++ painter/model.py
@@ -1,14 +1,30 @@
 # TODO: Add code here
 import math
+
+import matplotlib.pyplot as plt
+
+
 class Point:
     def __init__(self, x: float, y: float):
-        self.x: float = 0
-        self.y: float = 0
+        self.x: float = x
+        self.y: float = y
 
-class Circulo:
+class Circle:
     def __init__(self, center: Point, radius:float):
-        self.radius: float= 0
+        self.radius: float= radius
+        self.center: Point= center
+
 
     def area(self)->float:
-        Area = math.pi*self.radius
+        Area = math.pi*(self.radius)**2
+        return Area
+    def Draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+    def __str__(self):
+        Circle
+        with center at (x, y) and radius r
+        return  (f"{x}, {y}, {r}")
 
